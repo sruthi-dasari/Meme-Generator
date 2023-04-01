03198020b9758d181da01264afa01920993df84a
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-04-01 17:47:23 +0530

    Update styledComponents.js

--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 
 export const MemeBox = styled.div`
-  background-image: url('');
+  background-image: ${props => props.imageUrl};
+  height: 380px;
+  width: 100%;
+  background-size: cover;
+  margin-bottom: 30px;
 `
 export const TopAndBottomText = styled.p`
   font-size: 20px;
   color: #ffffff;
+  font-family: 'Open Sans';
 `
